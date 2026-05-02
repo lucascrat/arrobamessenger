49a84d09a5878e45da04eaf305dc19242d8c6603
HL Desenvolvedor <hldesenvolvedor@gmail.com>
2026-05-02 16:33:06 -0300

    feat: update api url for production

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,6 +2,6 @@ class Constants {
   // Substitua este IP pelo IP da sua máquina na rede local (ex: 192.168.0.10:3000) 
   // para testar em um celular físico via Wi-Fi.
   // Quando for fazer o deploy final, coloque a URL pública do seu servidor (ex: https://api.arroba.com)
-  static const String apiUrl = 'https://arroba.social.br';
-  static const String socketUrl = 'https://arroba.social.br';
+  static const String apiUrl = 'https://api.arroba.social.br';
+  static const String socketUrl = 'https://api.arroba.social.br';
 }
