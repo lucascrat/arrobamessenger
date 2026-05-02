da5f5597918a2024c2c3bebfc21fad73f9185787
HL Desenvolvedor <hldesenvolvedor@gmail.com>
2026-05-02 14:15:15 -0300

    chore: point PWA to Coolify backend URL

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -2,5 +2,6 @@ class Constants {
   // Substitua este IP pelo IP da sua máquina na rede local (ex: 192.168.0.10:3000) 
   // para testar em um celular físico via Wi-Fi.
   // Quando for fazer o deploy final, coloque a URL pública do seu servidor (ex: https://api.arroba.com)
-  static const String apiUrl = 'http://127.0.0.1:3000';
+  static const String apiUrl = 'http://b880g0cgscwc84scogkg0cwg.187.77.230.251.sslip.io';
+  static const String socketUrl = 'http://b880g0cgscwc84scogkg0cwg.187.77.230.251.sslip.io';
 }
